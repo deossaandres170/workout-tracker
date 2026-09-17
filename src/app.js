5d753cb756a4bebf809bf3ea5bc3ae78b4ecbb88
deossaandres170 <deossaandres170@gmail.com>
2026-09-17 13:58:39 -0500

    feat: configuracion de variables de entorno en el proyecto

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ console.log("workout-tracker app");
 
 const express = require("express"); // Import express
 const app = express(); // Create an instance of express
-const port = 8000; // puerto de escucha
+const { port } = require('./config/env'); // Import the port from the env file
 
 // Inicializacion del servidor y primera ruta
 app.get("/", (req, res) => {
