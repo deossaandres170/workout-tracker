2872d4011424421c21472406f3a0c3a4f4cb270f
deossaandres170 <deossaandres170@gmail.com>
2026-09-20 20:50:19 -0500

    feat: implementacion de metodos PUT, PATCH y DELETE para users y workouts

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,9 +1,23 @@
+// src/routes/v1/users.routes.js
 const express = require('express');
 const router = express.Router();
+const {
+  getUsers,
+  getUserById,
+  updateUser,
+  patchUser,
+  deleteUser
+} = require('../../controllers/users.controller');
 
-// GET /api/v1/users
-router.get('/', (req, res) => {
-  res.send('Get all users');
-});
+// GET
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+
+// PUT y PATCH
+router.put('/:id', updateUser);
+router.patch('/:id', patchUser);
+
+// DELETE
+router.delete('/:id', deleteUser);
 
 module.exports = router;
